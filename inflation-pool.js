bab607c4dba1205de87f454101db6e6e75b34773
lenondupe <lenondupe@gmail.com>
2017-12-20 11:18:18 +0100

    #1 add Ledger support

--- inflation-pool.js
+++ inflation-pool.js
@@ -1,10 +1,6 @@
 "use strict";
 
-var server = new StellarSdk.Server({
-	secure:	true,
-	hostname: 'horizon.stellar.org',
-	port: 443
-});
+var server = new StellarSdk.Server('https://horizon.stellar.org');
 StellarSdk.Network.usePublicNetwork();
 
 function isValidSeed(seed) {
@@ -31,4 +27,23 @@ function setInflationDest(seed, dest) {
 
 		return server.submitTransaction(tx);
 	});
+}
+
+function setInflationDestLedger(bip32Path, dest) {
+  return StellarLedger.comm.create_async().then(function(comm) {
+    var api = new StellarLedger.Api(comm);
+    return api.getPublicKey_async(bip32Path).then(function (result) {
+    	var publicKey = result.publicKey;
+			return server.loadAccount(publicKey).then(function (account) {
+        var tx = new StellarSdk.TransactionBuilder(account).addOperation(StellarSdk.Operation.setOptions({ inflationDest: dest })).build();
+        return api.signTx_async(bip32Path, tx).then(function (result) {
+          var keyPair = StellarSdk.Keypair.fromPublicKey(publicKey);
+          var hint = keyPair.signatureHint();
+          var decorated = new StellarSdk.xdr.DecoratedSignature({hint: hint, signature: result.signature});
+          tx.signatures.push(decorated);
+          return server.submitTransaction(tx);
+        })
+      })
+    })
+  });
 }
